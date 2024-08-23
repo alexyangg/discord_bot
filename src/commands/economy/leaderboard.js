56ebafa7ef89e187e82eb0994a69187d5349a8a5
Alex <ayang40@student.ubc.ca>
2024-08-22 20:33:25 -0700

    Add embed option to leaderboard command

--- src/commands/economy/leaderboard.js
+++ src/commands/economy/leaderboard.js
@@ -1,4 +1,4 @@
-const { Client, Interaction, AttachmentBuilder, GuildMember } = require("discord.js");
+const { Client, Interaction, ApplicationCommandOptionType, AttachmentBuilder, EmbedBuilder, } = require("discord.js");
 const { Font, LeaderboardBuilder } = require('canvacord');
 const Level = require("../../models/Level");
 const path = require('path');
@@ -16,6 +16,8 @@ module.exports = {
         }
 
         await interaction.deferReply();
+
+        const isEmbed = interaction.options.get('embed')?.value;
 
         const customFontPath = path.resolve(__dirname, '../../fonts/LeagueSpartan-Bold.ttf');
         Font.fromFileSync(customFontPath);
@@ -41,6 +43,7 @@ module.exports = {
         });
 
         // loop through the top users and fetch their data
+        // returns an array of arrays
         const userData = await Promise.all(topUsers.map(async (user, index) => {
             const guildMember = await interaction.guild.members.fetch(user.userId);
             return {
@@ -50,25 +53,71 @@ module.exports = {
                 level: user.level,
                 xp: user.xp,
                 rank: index + 1,
+                id: user.userId,
             };
         }));
 
-        const lb = new LeaderboardBuilder()
-            .setHeader({
-                title: userGuildName,
-                image: guildImage,
-                subtitle: `${guildNumMembers} members`,
-            })
-            .setPlayers(userData)
-            .setBackground(backgroundPath)
-            .setVariant('default');
+        if (isEmbed) {
+            // text-based leaderboard
+            let lbEmbed = new EmbedBuilder()
+                .setTitle(`**${userGuildName} Level Leaderboard**`)
+                .setColor('#ffffff')
+                .setFooter({ text: `${guildNumMembers} members` });
 
-        const image = await lb.build({ format: 'png' });
-        const attachment = new AttachmentBuilder(image);
-        interaction.editReply({ files: [attachment] });
+            let desc = "";
+
+            userData.forEach(user => {
+                let medal = "";
+                switch (user.rank) {
+                    case 1:
+                        medal = "🥇"; // gold medal for 1st place
+                        break;
+                    case 2:
+                        medal = "🥈"; // silver medal for 2nd place
+                        break;
+                    case 3:
+                        medal = "🥉"; // bronze medal for 3rd place
+                        break;
+                    default:
+                        medal = "🔹"; // diamond icon for others
+                }
+
+                desc += `${medal} <@${user.id}> Level: ${user.level} | XP: ${user.xp}\n`;
+            });
+
+            if (desc !== "") {
+                lbEmbed.setDescription(desc);
+                interaction.editReply({ embeds: [lbEmbed] });
+            } else {
+                interaction.editReply("I couldn't display the leaderboard.");
+                return;
+            }
+        } else {
+            // image-based leaderboard
+            const lb = new LeaderboardBuilder()
+                .setHeader({
+                    title: userGuildName,
+                    image: guildImage,
+                    subtitle: `${guildNumMembers} members`,
+                })
+                .setPlayers(userData)
+                .setBackground(backgroundPath)
+                .setVariant('default');
+
+            const image = await lb.build({ format: 'png' });
+            const attachment = new AttachmentBuilder(image);
+            interaction.editReply({ files: [attachment] });
+        }
     },
 
     name: 'leaderboard',
     description: "View the top users with the highest level.",
+    options: [
+        {
+            name: 'embed',
+            description: 'Displays the leaderboard as a text embed instead of an image.',
+            type: ApplicationCommandOptionType.Boolean,
+        }
+    ]
 
 };
