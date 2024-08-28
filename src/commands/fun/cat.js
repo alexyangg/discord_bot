9df2e49c953927e7bcfb557f3ac183e1c98827c2
Alex <ayang40@student.ubc.ca>
2024-08-28 12:31:30 -0700

    Implement /dog and /cat commands

---
+++ src/commands/fun/cat.js
@@ -0,0 +1,40 @@
+const { Client, CommandInteraction } = require('discord.js');
+const axios = require('axios');
+
+module.exports = {
+    name: 'cat',
+    description: 'Sends a random image of a cat.',
+
+    /**
+     * 
+     * @param {Client} client 
+     * @param {CommandInteraction} interaction 
+     */
+    callback: async (client, interaction) => {
+        try {
+            // fetch image from TheCatAPI
+            const response = await axios.get('https://api.thecatapi.com/v1/images/search',
+                {
+                    headers: {
+                        'x-api-key': process.env.CAT_API_KEY
+                    }
+                });
+            const catImageUrl = response.data[0].url;
+
+            await interaction.reply(
+                {
+                    content: "Here's a cute kitty for you!",
+                    files: [catImageUrl]
+                }
+            );
+        } catch (error) {
+            console.error(`Error fetching cat image: ${error}`);
+            await interaction.reply(
+                {
+                    content: "Sorry, I couldn't fetch a cat image right now. Please try again later.",
+                    ephemeral: true
+                }
+            );
+        }
+    }
+}
---
+++ src/commands/fun/dog.js
@@ -0,0 +1,40 @@
+const { Client, CommandInteraction } = require('discord.js');
+const axios = require('axios');
+
+module.exports = {
+    name: 'dog',
+    description: 'Sends a random image of a dog.',
+
+    /**
+     * 
+     * @param {Client} client 
+     * @param {CommandInteraction} interaction 
+     */
+    callback: async (client, interaction) => {
+        try {
+            // fetch image from TheDogAPI
+            const response = await axios.get('https://api.thedogapi.com/v1/images/search',
+                {
+                    headers: {
+                        'x-api-key': process.env.DOG_API_KEY
+                    }
+                });
+            const dogImageUrl = response.data[0].url;
+
+            await interaction.reply(
+                {
+                    content: 'Here\'s a cute doggo for you!',
+                    files: [dogImageUrl]
+                }
+            );
+        } catch (error) {
+            console.error(`Error fetching dog image: ${error}`);
+            await interaction.reply(
+                {
+                    content: "Sorry, I couldn't fetch a dog image right now. Please try again later.",
+                    ephemeral: true
+                }
+            );
+        }
+    }
+}
