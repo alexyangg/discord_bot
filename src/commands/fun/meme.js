fac85d6509dc78bf00a5b924aa7509ed2f970dbd
Alex <ayang40@student.ubc.ca>
2024-08-28 17:17:45 -0700

    Implement /meme command

---
+++ src/commands/fun/meme.js
@@ -0,0 +1,41 @@
+const { Client, CommandInteraction } = require('discord.js');
+const axios = require('axios');
+
+module.exports = {
+    name: 'meme',
+    description: 'Sends a random meme!',
+
+    /**
+     * 
+     * @param {Client} client 
+     * @param {CommandInteraction} interaction 
+     */
+    callback: async (client, interaction) => {
+        try {
+            // fetch image from Meme API
+            const response = await axios.get('https://meme-api.com/gimme');
+
+            if (response.data) {
+                const meme = response.data;
+
+                await interaction.reply(
+                    {
+                        content: meme.title,
+                        files: [meme.url]
+                    }
+                );
+            } else {
+                await interaction.reply("Sorry, I couldn't fetch a meme right now. Please try again later.");
+            }
+        } catch (error) {
+            console.log(`Error fetching meme: ${error}`);
+            await interaction.reply(
+                {
+                    content: "Sorry, I couldn't fetch a meme right now. Please try again later.",
+                    ephemeral: true
+                }
+            );
+        }
+    }
+
+}
