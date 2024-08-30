8150227bd43ab056337397c18f6bc60f5f735967
Alex <ayang40@student.ubc.ca>
2024-08-30 00:01:19 -0700

    Update bot invite link

--- src/commands/miscellaneous/invite.js
+++ src/commands/miscellaneous/invite.js
@@ -3,7 +3,7 @@ module.exports = {
     description: "Invite me to your server!",
 
     callback: async (client, interaction) => {
-        const inviteUrl = `https://discord.com/oauth2/authorize?client_id=859496216256970762&permissions=8&integration_type=0&scope=bot+applications.commands`;
+        const inviteUrl = `https://discord.com/oauth2/authorize?client_id=859496216256970762`;
 
         await interaction.reply({
             content: `Here is my [invite link](${inviteUrl})!`,
